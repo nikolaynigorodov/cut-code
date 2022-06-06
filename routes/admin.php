0985bb58e78c11495845a9cb09fd130d3f650177
Nikolay <nikolaynigorodov@gmail.com>
2022-06-06 12:13:28 +0300

    AdminPanel create routing

--- routes/admin.php
+++ routes/admin.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Admin\PostController;
-use App\Models\AdminUser;
 use Illuminate\Support\Facades\Route;
 
-Route::get('login', [AdminUser::class, 'index'])->name('admin.login');
-Route::get('login_process', [AdminUser::class, 'login'])->name('admin.login_process');
+Route::get('login', [AuthController::class, 'index'])->name('admin.login');
+Route::get('login_process', [AuthController::class, 'login'])->name('admin.login_process');
 
 Route::middleware('auth:admin')->group(function () {
     Route::resource('posts', PostController::class);
